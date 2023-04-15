5db4c3e2c16a46a63d5f45e07bc60e6ac45c8e52
FINIKKKK <digitallkrash@gmail.com>
2023-04-15 16:07:06 +0600

    init

---
+++ app/Http/Controllers/MyPlaceController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class MyPlaceCOntroller extends Controller
+{
+    public function index() {
+        return 'this in my page';
+    }
+}
