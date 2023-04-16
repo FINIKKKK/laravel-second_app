cf2aca7dceb02dbbdd2b746106d1c73e358be90f
FINIKKKK <digitallkrash@gmail.com>
2023-04-17 01:09:56 +0600

    pt2

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\MyPlaceController;
+use App\Http\Controllers\PostController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,4 +18,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/hello', [MyPlaceController::class, 'index']);
+Route::get('/posts', [PostController::class, 'index']);
